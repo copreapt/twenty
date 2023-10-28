bc3d85716a71bcac68e676153cbf19ca4ebfea7a
Oprea Catalin <copreapt@gmail.com>
2023-10-28 20:43:06 +0300

    more fixes

--- src/pages/dashboard/SharedLayout.tsx
+++ src/pages/dashboard/SharedLayout.tsx
@@ -3,12 +3,12 @@ import { Navbar, AddsSection, PostsSection, SearchBar } from "../../components"
 
 const SharedLayout = () => {
   return (
-    <main className="h-screen md:w-full lg:max-w-screen-2xl lg:center-div-lg bg-gray-200 flex flex-col">
-      <div className="pt-10 grow-0">
+    <main className="h-screen md:w-full lg:max-w-screen-2xl lg:center-div-lg bg-gray-200 flex flex-col overflow-y-hidden">
+      <div className="pt-10 flex-end justify-start">
         <SearchBar />
       </div>
       {/* container */}
-      <div className="flex justify-between mt-10 mx-3 overflow-y-auto ">
+      <div className="flex justify-between mt-10 mx-3 overflow-y-auto grow">
 {/* Navbar for desktop version */}
           <div className="hidden md:flex">
             <Navbar />
@@ -23,9 +23,9 @@ const SharedLayout = () => {
           </div>
       </div>
 {/* Navbar for mobile version */}
-      <div className="md:hidden mx-5 flex items-center justify-center py-3 mt-10 bg-white rounded-md shadow-sm shadow-white mb-3">
+      <div className="md:hidden mx-5 flex items-center py-3 mt-10 bg-white rounded-md shadow-sm shadow-white mb-3 justify-end">
         <Navbar />
-      </div>
+      </div>      
     </main>
   )
 }
