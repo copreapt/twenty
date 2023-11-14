f97042fa8fe73047272a294f29da32b04ca6f137
Oprea Catalin <copreapt@gmail.com>
2023-11-15 00:29:15 +0200

    axios fixes

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const customFetch = axios.create({
   baseURL: "https://twenty-myserver.onrender.com/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
 export default customFetch;
