8d38154100e9e88e49c3c3bfa4da4b00193ff270
Oprea Catalin <copreapt@gmail.com>
2023-12-04 00:46:11 +0200

    fix store

--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/user/userSlice";
 import postsSlice from "./features/posts/postSlice";
+import likesSlice from "./features/likes/likesSlice";
 
 export const store = configureStore({
     reducer:{
         user: userSlice,
         posts: postsSlice,
+        likes: likesSlice,
     }
 })
