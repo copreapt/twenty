d70e0c5d9ca5f3230b16914abae1ff982a73a16e
Oprea Catalin <copreapt@gmail.com>
2023-12-06 00:17:49 +0200

    fix

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -127,7 +127,7 @@ const PostCard = () => {
                 and <span className="text-md text-cyan-600">Others</span>
               </span>
               {/* last comment */}
-              <span className="text-lg">Last comment here</span>
+              <span className="text-xl">Last comment here</span>
               {/* comment input */}
               <input
                 type="text"
