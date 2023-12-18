bd7e4def1361a2ea38e8db74c1a88eb9c4d04c11
Oprea Catalin <copreapt@gmail.com>
2023-12-19 01:27:26 +0200

    fix

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="p-5 m-auto max-w-[1170px] w-[90vw]">
-      <div className="loading"></div>
+      <div className="w-[2rem] h-[2rem] mt-[4rem] mx-auto rounded-full border-2 border-gray-300 border-t-2 border-t-cyan-700 animate-spin"></div>
     </div>
   );
 };
