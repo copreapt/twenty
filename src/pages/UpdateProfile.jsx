feb32a3be6afe97c1d92509bff3ea65f56c0bf6a
Oprea Catalin <copreapt@gmail.com>
2023-12-20 22:35:15 +0200

    fix

--- src/pages/UpdateProfile.jsx
+++ src/pages/UpdateProfile.jsx
@@ -52,16 +52,16 @@ const UpdateProfile = () => {
   };
 
   return (
-    <form
-      className="md:w-full md:mx-auto h-screen bg-gray-200 flex flex-col absolute items-center"
-      action="submit"
-    >
+    <div className="md:w-full md:mx-auto h-screen bg-gray-200 flex flex-col absolute items-center">
       {/* navbar div */}
       <div className="hidden md:flex bg-white overflow-hidden fixed top-0 z-0 w-full">
         <SearchBar />
       </div>
       {/* container */}
-      <div className="grid grid-cols-12 py-14 md:max-w-3xl md:mx-auto lg:max-w-screen-lg lg:gap-20 xl:max-w-screen-xl md:mt-10">
+      <form
+        className="grid grid-cols-12 py-14 md:max-w-3xl md:mx-auto lg:max-w-screen-lg lg:gap-20 xl:max-w-screen-xl md:mt-10"
+        action="submit"
+      >
         {/* Profile Picture */}
         <div className="flex flex-col items-center space-y-10 col-span-5">
           <div className="flex justify-center items-center h-[400px] w-[400px] rounded-full overflow-hidden">
@@ -118,12 +118,16 @@ const UpdateProfile = () => {
             labelText="full name"
           />
         </div>
-      </div>
+        <button
+          className="bg-cyan-500 px-20 py-2 text-white rounded-lg hover:bg-cyan-700 ease-in-out duration-700 hover:cursor-pointer"
+          type="submit"
+          onClick={onSubmit}
+        >
+          Save changes
+        </button>
+      </form>
       {/* save button */}
-      <div className="bg-cyan-500 px-20 py-2 text-white rounded-lg hover:bg-cyan-700 ease-in-out duration-700 hover:cursor-pointer">
-        <button type="submit" onClick={onSubmit}>Save changes</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
