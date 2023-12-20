dd6cba7d2a244f1d32924fba2b4c769c8cd74878
Oprea Catalin <copreapt@gmail.com>
2023-12-20 22:08:15 +0200

    fix

--- src/components/FormRow.jsx
+++ src/components/FormRow.jsx
@@ -5,7 +5,7 @@ const FormRow = ({type, name, value, handleChange, labelText}) => {
           {labelText || name}
         </label>
         <input
-          placeholder={labelText || name}
+          placeholder={value}
           id={name}
           type={type}
           name={name}
